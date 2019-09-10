e5f75ea30863e347211b412c53361814dea8406a
PavelNestor <pavelnesterets82@gmail.com>
2019-09-10 15:33:08 +0300

    add main slider functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,44 @@
 "use strict";
 
-(function() {
-  var menuImg = document.getElementById("menu-image");
-  var menuClose = document.getElementById("menu-close");
-  var menuContent = document.getElementById("menu-content");
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-  const onToogleMenu = () => {
-    menuContent.classList.toggle("menu-show");
-  };
+const slides = $$('.slides');
+const carLinks = $$('.car-links');
+const prevSlide = $('#prev-slide');
+const nextSlide = $('#next-slide');
 
-  menuImg.addEventListener("click", onToogleMenu);
-  menuImg.addEventListener("touch", onToogleMenu);
-  menuClose.addEventListener("click", onToogleMenu);
-})();
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function nextSlides() {
+  showSlides((slideIndex += 1));
+}
+
+function currentSlide(index) {
+  showSlides(slideIndex = index);
+}
+
+function prewSlides() {
+  showSlides((slideIndex += -1));
+}
+
+function showSlides(index) {
+  if (index > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (index < 1) {
+    slideIndex = slides.length;
+  }
+
+  slides.forEach(slide => slide.classList.add('hidden'));
+  carLinks.forEach(carLink => carLink.classList.remove('car-links_active'));
+  slides[slideIndex - 1].classList.remove('hidden');
+  carLinks[slideIndex - 1].classList.add('car-links_active');
+  carLinks[slideIndex - 1].removeEventListener('click', currentSlide)
+}
+
+prevSlide.addEventListener('click', prewSlides);
+nextSlide.addEventListener('click', nextSlides);
+carLinks.forEach((carLink, index) => carLink.addEventListener('click', () => currentSlide(index + 1)));
