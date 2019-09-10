c2a7103f7c0973158b6a79669a7d6dc3fbbe693e
PavelNestor <pavelnesterets82@gmail.com>
2019-09-10 17:27:10 +0300

    feature/add-slider-logick

--- src/js/main.js
+++ src/js/main.js
@@ -3,42 +3,58 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const slides = $$('.slides');
-const carLinks = $$('.car-links');
+// const slides = $$('.slides');
+// const carLinks = $$('.car-links');
 const prevSlide = $('#prev-slide');
 const nextSlide = $('#next-slide');
 
-let slideIndex = 1;
-showSlides(slideIndex);
+let slidesIndex = [1, 1];
+var slidesId = ['main-slides', 'lamb-slides']
+var linksId = ['main-links', 'lamb-links']
 
-function nextSlides() {
-  showSlides((slideIndex += 1));
+showSlides(1, 0);
+
+function nextSlides(id) {
+  showSlides((slidesIndex[id] += 1), id);
 }
 
-function currentSlide(index) {
-  showSlides(slideIndex = index);
+function currentSlide(index, id) {
+  let res = slidesIndex[id] = index
+  showSlides(res, id);
 }
 
-function prewSlides() {
-  showSlides((slideIndex += -1));
+function prewSlides(id) {
+  showSlides((slidesIndex[id] += -1), id);
 }
 
-function showSlides(index) {
-  if (index > slides.length) {
-    slideIndex = 1;
+function showSlides(index, slideId) {
+  console.log('slidesIndex', slidesIndex);
+  
+  const currentSlides = $$(`.${slidesId[slideId]}`);
+  console.log('slideId', index);
+  
+  const currentLinks = $$(`.${linksId[slideId]}`);
+
+  console.log('index > currentSlides.length', currentSlides.length);
+  
+  
+  if (index > currentSlides.length) {
+    slidesIndex[slideId] = 1;
+  } else if (index < 1){
+    slidesIndex[slideId] = currentSlides.length -1;
+  } else {
+    slidesIndex[slideId] = index;
   }
 
-  if (index < 1) {
-    slideIndex = slides.length;
-  }
+  currentSlides.forEach(slide => slide.classList.add('hidden'));
+  currentLinks.forEach(link => link.classList.remove(`${linksId[slideId]}_active`));
+  
+  currentLinks.forEach((carLink, index) => carLink.addEventListener('click', () => currentSlide(index + 1, slideId)));
 
-  slides.forEach(slide => slide.classList.add('hidden'));
-  carLinks.forEach(carLink => carLink.classList.remove('car-links_active'));
-  slides[slideIndex - 1].classList.remove('hidden');
-  carLinks[slideIndex - 1].classList.add('car-links_active');
-  carLinks[slideIndex - 1].removeEventListener('click', currentSlide)
+  currentSlides[slidesIndex[slideId] - 1].classList.remove('hidden');
+  currentLinks[slidesIndex[slideId] - 1].classList.add(`${linksId[slideId]}_active`);
+  currentLinks[slidesIndex[slideId] - 1].removeEventListener('click', currentSlide)
 }
 
-prevSlide.addEventListener('click', prewSlides);
-nextSlide.addEventListener('click', nextSlides);
-carLinks.forEach((carLink, index) => carLink.addEventListener('click', () => currentSlide(index + 1)));
+prevSlide.addEventListener('click', () => prewSlides(0));
+nextSlide.addEventListener('click', () => nextSlides(0));
